28ad8c7bd771d80c1c6be70d9f550394691471c2
Adxtyxn <adithyanedu1209@gmail.com>
2026-04-27 21:56:47 +0530

    weather api integration and search feature

---
+++ src/services/weatherservice.js
@@ -0,0 +1,19 @@
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+export const fetchWeather = async (city) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
